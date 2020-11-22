f2d367315965ad67220eeb1be86dcb3731401ce5
Christopher Inman <christophereinman@gmail.com>
2020-11-22 12:22:07 -0600

    More recommended approach to state objects

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ const App = () => {
   ]);
 
   const addNewGoalHandler = (newGoal) => {
-    setCourseGoals(courseGoals.concat(newGoal));
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
   };
 
   return (
