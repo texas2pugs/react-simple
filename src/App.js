2cb945e5d6aeb76dd5355db02e72b9953780625b
Christopher Inman <christophereinman@gmail.com>
2020-11-22 11:39:35 -0600

    Using props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import GoalList from './components/GoalList';
 import './App.css';
 
 const App = () => {
+  const courseGoals = [
+    { id: 'cg1', text: 'Finish the course' },
+    { id: 'cg2', text: 'Learn main concepts' },
+    { id: 'cg3', text: 'Help others' },
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 import './GoalList.css';
 
-const GoalList = () => {
-  return (
-    <ul className="goal-list">
-      <li>Finish the course</li>
-      <li>Learn main topics</li>
-      <li>Help others</li>
-    </ul>
-  );
+const GoalList = (props) => {
+  console.log(props.goals);
+  return <ul className="goal-list"></ul>;
 };
 
 export default GoalList;
