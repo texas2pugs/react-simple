5cb695093eeba7ab790782cfc4a953677d603521
Christopher Inman <christophereinman@gmail.com>
2020-11-22 11:46:43 -0600

    Rendering list items

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -3,8 +3,13 @@ import React from 'react';
 import './GoalList.css';
 
 const GoalList = (props) => {
-  console.log(props.goals);
-  return <ul className="goal-list"></ul>;
+  return (
+    <ul className="goal-list">
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default GoalList;
