22ba086be2a61ce27cf7c07d9d297d124f555761
Christopher Inman <christophereinman@gmail.com>
2020-11-22 11:24:24 -0600

    Split into components

---
+++ src/components/GoalList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './GoalList.css';
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish the course</li>
+      <li>Learn main topics</li>
+      <li>Help others</li>
+    </ul>
+  );
+};
+
+export default GoalList;
